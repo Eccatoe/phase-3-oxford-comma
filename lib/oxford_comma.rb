bf609c3e6c3a893cbef49e916a76f6abc2dcf5fb
Elizabeth Catoe <eccatoe2517@gmail.com>
2022-02-22 18:28:51 -0500

    commit tree e2c89a20e1664a83949eee47f3a50ff0dfb97d04

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
-def oxford_comma(array)
+# oxford_comma(["fiddleheads", "okra", "kohlrabi"])
+# # => "fiddleheads, okra, and kohlrabi"
 
+require 'pry'
+
+def oxford_comma(array)
+   return array.join(" and ") if array.length<3
+
+    array[-1]="and #{array[-1]}"
+    array.join(", ")
 end
+
+binding.pry
+
+oxford_comma(["chicken", "pork", "beef", "lamb", "goat"])
